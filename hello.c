e7f4095c397750539a8472a50da2c0ebab1896b6
Luke-the-Husker <lmcgrew3@huskers.unl.edu>
2024-08-29 09:20:05 -0500

    partner change

--- hello.c
+++ hello.c
@@ -8,6 +8,8 @@ int main(int argc, char **argv){
 
     printf("Hello, I'm Elijah Sebey!\n");
     printf("Major is Computer Engineering!\n");
+    printf("Hello, I'm Luke McGrew!\n");
+    printf("Major is General Engineering!\n");
 
     return 0;
 }
