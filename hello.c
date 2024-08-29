a94d3e64260361da5ac582a3f7767ff3e8648399
Elijah Sebey <esebey2@huskers.unl.edu>
2024-08-29 07:36:36 -0600

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv){
+
+    printf("Hello, I'm Elijah Sebey!\n");
+    printf("Major is Computer Engineering!\n");
+
+    return 0;
+}
